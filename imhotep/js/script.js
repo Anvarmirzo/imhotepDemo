80e0373fbb6ac6ddd7406115ea5e35cdf366bffe
Anvar <www.latvienhoasadrizeges@gmail.com>
2021-03-25 17:17:24 +0500

    full edited

--- imhotep/js/script.js
+++ imhotep/js/script.js
@@ -82,48 +82,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  showNumberForm(); // /* Offer slider */
-  // const offerSlider = new Swiper('.offer-slider-container', {
-  //     slidesPerView: 3,
-  //     loop: true,
-  //     breakpoints: {
-  //         // when window width is >= 320px
-  //         320: {
-  //             slidesPerView: 1.5,
-  //             spaceBetween: 20,
-  //             autoplay: {
-  //                 delay: 2000,
-  //                 disableOnInteraction: false
-  //             },
-  //         },
-  //         // when window width is >= 480px
-  //         480: {
-  //             slidesPerView: 1.5,
-  //             spaceBetween: 30,
-  //             autoplay: {
-  //                 delay: 2000,
-  //                 disableOnInteraction: false
-  //             },
-  //         },
-  //         // when window width is >= 768px
-  //         768: {
-  //             slidesPerView: 2,
-  //             spaceBetween: 40,
-  //             autoplay: {
-  //                 delay: 2000,
-  //                 disableOnInteraction: false
-  //             },
-  //         },
-  //         // when window width is >= 992px
-  //         992: {
-  //             slidesPerView: 3,
-  //             spaceBetween: 20,
-  //             autoplay: false,
-  //             loop: false,
-  //         }
-  //     }
-  // });
-
+  showNumberForm();
   /* Gallery slider */
 
   var gallerySlider = new Swiper('.gallery-slider-container', {
@@ -152,58 +111,26 @@ document.addEventListener('DOMContentLoaded', function () {
         slidesPerView: 3
       }
     }
-  }); // /* Advantage slider */
-  // const advantageSlider = new Swiper('.advantage-slider-container', {
-  //     spaceBetween: 21,
-  //     slidesPerView: 3,
-  //     breakpoints: {
-  //         320: {
-  //             slidesPerView: 1,
-  //         },
-  //         480: {
-  //             slidesPerView: 1,
-  //         },
-  //         768: {
-  //             slidesPerView: 2,
-  //         },
-  //         991: {
-  //             slidesPerView: 3,
-  //         },
-  //     },
-  //     // breakpointsBase: 'container'
-  // });
-  // /* Questions slider */
-  // const questionsSlider = new Swiper('.questions-slider-container', {
-  //     slidesPerView: 3,
-  //     spaceBetween: 20,
-  //     breakpoints: {
-  //         // when window width is >= 320px
-  //         320: {
-  //             slidesPerView: 1,
-  //             loop: true,
-  //             autoplay: {
-  //                 delay: 3000,
-  //                 disableOnInteraction: false
-  //             },
-  //         },
-  //         // when window width is >= 768px
-  //         768: {
-  //             slidesPerView: 2,
-  //             loop: true,
-  //             autoplay: {
-  //                 delay: 3000,
-  //                 disableOnInteraction: false
-  //             },
-  //         },
-  //         // when window width is >= 1200px
-  //         1200: {
-  //             slidesPerView: 3,
-  //             spaceBetween: 20,
-  //             loop: false,
-  //             autoplay: false,
-  //         }
-  //     }
-  // });
+  });
+  /* Objects slider */
+
+  var galleryThumbs = new Swiper('.objects-gallery-thumbs', {
+    spaceBetween: 22,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true
+  });
+  var galleryTop = new Swiper('.objects-gallery-top', {
+    spaceBetween: 22,
+    thumbs: {
+      swiper: galleryThumbs
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
 
   function setMaxHeight(getSelector) {
     var els = document.querySelectorAll(getSelector),
--- src/js/swiper-sliders-config.js
+++ src/js/swiper-sliders-config.js
@@ -1,45 +1,3 @@
-// /* Offer slider */
-// const offerSlider = new Swiper('.offer-slider-container', {
-//     slidesPerView: 3,
-//     loop: true,
-//     breakpoints: {
-//         // when window width is >= 320px
-//         320: {
-//             slidesPerView: 1.5,
-//             spaceBetween: 20,
-//             autoplay: {
-//                 delay: 2000,
-//                 disableOnInteraction: false
-//             },
-//         },
-//         // when window width is >= 480px
-//         480: {
-//             slidesPerView: 1.5,
-//             spaceBetween: 30,
-//             autoplay: {
-//                 delay: 2000,
-//                 disableOnInteraction: false
-//             },
-//         },
-//         // when window width is >= 768px
-//         768: {
-//             slidesPerView: 2,
-//             spaceBetween: 40,
-//             autoplay: {
-//                 delay: 2000,
-//                 disableOnInteraction: false
-//             },
-//         },
-//         // when window width is >= 992px
-//         992: {
-//             slidesPerView: 3,
-//             spaceBetween: 20,
-//             autoplay: false,
-//             loop: false,
-//         }
-//     }
-// });
-
 /* Gallery slider */
 const gallerySlider = new Swiper('.gallery-slider-container', {
     slidesPerView: 3,
@@ -68,58 +26,21 @@ const gallerySlider = new Swiper('.gallery-slider-container', {
         },
     },
 });
-
-// /* Advantage slider */
-// const advantageSlider = new Swiper('.advantage-slider-container', {
-//     spaceBetween: 21,
-//     slidesPerView: 3,
-//     breakpoints: {
-//         320: {
-//             slidesPerView: 1,
-//         },
-//         480: {
-//             slidesPerView: 1,
-//         },
-//         768: {
-//             slidesPerView: 2,
-//         },
-//         991: {
-//             slidesPerView: 3,
-//         },
-//     },
-//     // breakpointsBase: 'container'
-
-// });
-
-// /* Questions slider */
-// const questionsSlider = new Swiper('.questions-slider-container', {
-//     slidesPerView: 3,
-//     spaceBetween: 20,
-//     breakpoints: {
-//         // when window width is >= 320px
-//         320: {
-//             slidesPerView: 1,
-//             loop: true,
-//             autoplay: {
-//                 delay: 3000,
-//                 disableOnInteraction: false
-//             },
-//         },
-//         // when window width is >= 768px
-//         768: {
-//             slidesPerView: 2,
-//             loop: true,
-//             autoplay: {
-//                 delay: 3000,
-//                 disableOnInteraction: false
-//             },
-//         },
-//         // when window width is >= 1200px
-//         1200: {
-//             slidesPerView: 3,
-//             spaceBetween: 20,
-//             loop: false,
-//             autoplay: false,
-//         }
-//     }
-// });
+/* Objects slider */
+const galleryThumbs = new Swiper('.objects-gallery-thumbs', {
+    spaceBetween: 22,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+const galleryTop = new Swiper('.objects-gallery-top', {
+    spaceBetween: 22,
+    thumbs: {
+        swiper: galleryThumbs
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
